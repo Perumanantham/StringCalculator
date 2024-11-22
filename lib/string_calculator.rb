5424b1c658d477c821391a6eafbeea2871940f88
Perumanantham <37172323+Perumanantham@users.noreply.github.com>
2024-11-22 23:12:32 +0530

    Feature: handle the sum of multiple numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,16 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-  
-    nums = numbers.split(/,|\n/).map(&:to_i)
+    return numbers.to_i if numbers.match?(/^\d+$/)
+
+    delimiter = /,|\n/
+
+    nums = numbers.split(delimiter).map(&:to_i)
+
     negatives = nums.select { |n| n.negative? }
   
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   
     nums.sum
-  end    
+  end
 end
