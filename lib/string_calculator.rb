2fb2d95e9656f20e54b8c3cca04986e8a7279396
Perumanantham <37172323+Perumanantham@users.noreply.github.com>
2024-11-22 23:25:23 +0530

    Feature : new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,19 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.match?(/^\d+$/)
 
+    # Default delimiter: comma and newline
     delimiter = /,|\n/
 
+    # Split the numbers using the delimiter and map to integers
     nums = numbers.split(delimiter).map(&:to_i)
 
+    # Check for negative numbers
     negatives = nums.select { |n| n.negative? }
   
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   
+    # Return the sum of all numbers
     nums.sum
   end
 end
